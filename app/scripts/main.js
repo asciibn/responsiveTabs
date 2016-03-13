fdfee0d4edb4c5f3372ce5c718963849a35cc934
Russ Ferguson <rferguson@technologycoach.com>
2016-03-12 23:04:31 -0500

    renamed stylesheets. added JS for accordian functions

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,18 @@
-console.log('\'Allo \'Allo!');
+(function() {
+
+  var mediaQueryInfo = window.matchMedia('(max-width: 800px)');
+      mediaQueryInfo.addEventListener(setTabEvents);
+
+      function setTabEvents(){
+         if(mediaQueryInfo.matches){
+           return true;
+         }else{
+           return false;
+         }
+      }
+  document.getElementById('tabItemList').addEventListener('click', function(e){
+      //console.log(e.target);
+      console.log(setTabEvents())
+  });
+
+})();
