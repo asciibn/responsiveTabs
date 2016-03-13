6d05c53c6bc33f80ad52c4cad6546b66e442903e
Russ Ferguson <rferguson@technologycoach.com>
2016-03-13 17:59:33 -0400

    accordian working. need to  add styles and remove items from list when going back to desktop mode

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,22 +1,25 @@
+
 (function() {
+'use strict'
+
+var targetContent;
+var targetElement;
+
 
   var mediaQueryInfo = window.matchMedia('(max-width: 800px)');
       mediaQueryInfo.addEventListener(setTabEvents);
 
     function setTabEvents(){
+
        if(mediaQueryInfo.matches){
          return true;
        }else{
+        console.log('larget than 800px');
          return false;
        }
-    }
+    };
 
   document.getElementById('tabItemList').addEventListener('click', function(e){
-    var targetContent;
-    var targetElement;
-    var docFragment = document.createDocumentFragment();
-    var tempDiv = document.createElement('div');
-    var selectedTab;
 
     if(e.target.rel === undefined){
       targetContent = e.target.parentElement.rel;
@@ -33,40 +36,47 @@
   if(setTabEvents()){
     switch (targetContent) {
       case 'item1':
-
+      removeFragments();
       if(targetElement.parentElement.childNodes.length != 2){
-        tempDiv.appendChild(document.getElementById('item1'))
-        docFragment.appendChild(tempDiv);
-        targetElement.parentElement.appendChild(docFragment);
+        addNewElement(document.getElementById('item1'));
       }
-            //console.log(document.getElementById('item1'));
-            //console.log(targetElement.parentElement.childNodes.length);
-            //console.log(selectedTab.childNodes[0]);
-        break;
+
+      break;
       case 'item2':
+        removeFragments();
+        if(targetElement.parentElement.childNodes.length != 2){
+          addNewElement(document.getElementById('item2'));
+        }
 
-      if(targetElement.parentElement.childNodes.length != 2){
-        tempDiv.appendChild(document.getElementById('item2'))
-        docFragment.appendChild(tempDiv);
-        targetElement.parentElement.appendChild(docFragment);
-      }
-      //selectedTab.removeChild(docFragment);
-          //console.log(document.getElementById('item2'));
-        break;
+      break;
       case 'item3':
-      if(targetElement.parentElement.childNodes.length != 2){
-        tempDiv.appendChild(document.getElementById('item3'))
-        docFragment.appendChild(tempDiv);
-        targetElement.parentElement.appendChild(docFragment);
-      }
-        break;
+        removeFragments();
+        if(targetElement.parentElement.childNodes.length != 2){
+          addNewElement(document.getElementById('item3'));
+        }
+      break;
       default:
 
     }
   }
+}.bind(this));
 
+function addNewElement(content){
+  var tempDiv = document.createElement('div');
+  var docFragment = document.createDocumentFragment();
+    tempDiv.appendChild(content);
+    docFragment.appendChild(tempDiv);
+    targetElement.parentElement.appendChild(docFragment);
+}
 
-
-  });
+  function removeFragments(){
+    var navList = document.getElementById('tabItemList');
+    for(var i = 0; i < navList.childNodes.length; i++){
+      //console.log(navList.childNodes[i] +' ' + navList.childNodes[i].nodeType);
+      if(navList.childNodes[i].nodeType === 1 && navList.childNodes[i].childNodes[1] !== undefined){
+        document.querySelector('.items').appendChild(navList.childNodes[i].childNodes[1]);
+      }
+    }
+  }
 
 })();
